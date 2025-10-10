e10442b4626310faf540df18c6f47c2250f471e0
Hamza-Khan-official <*hamza@79034#>
2025-10-10 15:18:07 +0500

    updated

--- mouse_detailed/mouse_deatiled_page.js
+++ mouse_detailed/mouse_deatiled_page.js
@@ -1,5 +1,188 @@
 
 
+
+
+var product_detailed = [
+    {
+        "id": 1,
+        "name": "Mouse",
+        "description": "A stylish smartwatch with heart rate monitor.",
+        "color": "black",
+        "price": "$50",
+        "image": "images/images.jpg",
+
+        "first_mouse_carosel_first_image": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image": "../images/mouse/mouse_1.4.jpg",
+
+        "first_mouse_carosel_first_image_": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image_": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image_": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image_": "../images/mouse/mouse_1.4.jpg",
+
+
+        "Model": "FG12, FG12S",
+        "Type": "Wireless",
+        "Connection": "2.4G Hz",
+        "Operating Range": "10-15m",
+        "Ergonomic Design": "Symmetric",
+        "Sensor": "Optical",
+        "Resolution": "1200 DPI",
+        "Report Rate": "125Hz",
+        "Buttons No": "3",
+        "Receiver": "Nano USB Receiver",
+        "Battery": "1AA Alkaline Battery",
+        "Size": "108(L) x 64(W) x 35(H) mm",
+        "Net Weight": "83 g (w/ battery)",
+        "System Requirements": "Windows 10/11",
+
+        "first_mouse_Qunatity": "2",
+        "first_mouse_orignal_price": "$1099",
+        "first_mouse_cut_price": "1299",
+    },
+
+    // id_2
+    {
+        "id": 2,
+        "name": "Mouse_3",
+        "description": "Mouse 3 Stylish.",
+        "color": "black",
+        "price": "$50",
+
+        "first_mouse_carosel_first_image": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image": "../images/mouse/mouse_1.4.jpg",
+
+        "first_mouse_carosel_first_image_": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image_": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image_": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image_": "../images/mouse/mouse_1.4.jpg",
+
+
+        "Model": "FG12, FG12S",
+        "Type": "Wireless",
+        "Connection": "2.4G Hz",
+        "Operating Range": "10-15m",
+        "Ergonomic Design": "Symmetric",
+        "Sensor": "Optical",
+        "Resolution": "1200 DPI",
+        "Report Rate": "125Hz",
+        "Buttons No": "3",
+        "Receiver": "Nano USB Receiver",
+        "Battery": "1AA Alkaline Battery",
+        "Size": "108(L) x 64(W) x 35(H) mm",
+        "Net Weight": "83 g (w/ battery)",
+        "System Requirements": "Windows 10/11",
+
+        "first_mouse_Qunatity": "2",
+        "first_mouse_orignal_price": "$1099",
+        "first_mouse_cut_price": "1299",
+    },
+
+    // id_3
+    {
+        "id": 3,
+        "name": "Mouse_5",
+        "description": "Mouse 5 Stylish.",
+        "color": "black",
+        "price": "$50",
+
+        "first_mouse_carosel_first_image": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image": "../images/mouse/mouse_1.4.jpg",
+
+        "first_mouse_carosel_first_image_": "../images/mouse/mouse_1.1.jpg",
+        "first_mouse_carosel_second_image_": "../images/mouse/mouse_1.2.jpg",
+        "first_mouse_carosel_third_image_": "../images/mouse/mouse_1.3.jpg",
+        "first_mouse_carosel_fourth_image_": "../images/mouse/mouse_1.4.jpg",
+
+
+        "Model": "FG12, FG12S",
+        "Type": "Wireless",
+        "Connection": "2.4G Hz",
+        "Operating Range": "10-15m",
+        "Ergonomic Design": "Symmetric",
+        "Sensor": "Optical",
+        "Resolution": "1200 DPI",
+        "Report Rate": "125Hz",
+        "Buttons No": "3",
+        "Receiver": "Nano USB Receiver",
+        "Battery": "1AA Alkaline Battery",
+        "Size": "108(L) x 64(W) x 35(H) mm",
+        "Net Weight": "83 g (w/ battery)",
+        "System Requirements": "Windows 10/11",
+
+        "first_mouse_Qunatity": "2",
+        "first_mouse_orignal_price": "$1099",
+        "first_mouse_cut_price": "1299",
+
+
+    },
+];
+
+// 🟩 Step 2: URL se product id lena
+const urlParams = new URLSearchParams(window.location.search);
+const productId = parseInt(urlParams.get("id")); // e.g. ?id=2
+// 🟩 Step 3: Specific product find karna
+const firstProduct = product_detailed.find(p => p.id === productId);
+// 🟩 Step 4: Agar product mil gaya to data show karo
+if (firstProduct) {
+
+document.getElementById("first_mouse_name").innerText = firstProduct.name;
+document.getElementById("first_mouse_des").innerText = firstProduct.description;
+document.getElementById("first_mouse_color").innerText = firstProduct.color;
+document.getElementById("Model").innerText = firstProduct.Model;
+document.getElementById("Type").innerText = firstProduct.Type;
+document.getElementById("Connection").innerText = firstProduct.Connection;
+document.getElementById("Operating Range").innerText = firstProduct["Operating Range"];
+document.getElementById("Ergonomic Design").innerText = firstProduct["Ergonomic Design"];
+document.getElementById("Sensor").innerText = firstProduct.Sensor;
+document.getElementById("Resolution").innerText = firstProduct.Resolution;
+document.getElementById("Report Rate").innerText = firstProduct["Report Rate"];
+document.getElementById("Buttons No").innerText = firstProduct["Buttons No"];
+document.getElementById("Receiver").innerText = firstProduct.Receiver;
+document.getElementById("Battery").innerText = firstProduct.Battery;
+document.getElementById("Size").innerText = firstProduct.Size;
+document.getElementById("Net Weight").innerText = firstProduct["Net Weight"];
+document.getElementById("System Requirements").innerText = firstProduct["System Requirements"];
+
+document.getElementById("first_mouse_carosel_first_image").src = firstProduct.first_mouse_carosel_first_image;
+document.getElementById("first_mouse_carosel_second_image").src = firstProduct.first_mouse_carosel_second_image;
+document.getElementById("first_mouse_carosel_third_image").src = firstProduct.first_mouse_carosel_third_image;
+document.getElementById("first_mouse_carosel_fourth_image").src = firstProduct.first_mouse_carosel_fourth_image;
+
+document.getElementById("first_mouse_carosel_first_image_").src = firstProduct.first_mouse_carosel_first_image_;
+document.getElementById("first_mouse_carosel_second_image_").src = firstProduct.first_mouse_carosel_second_image_;
+document.getElementById("first_mouse_carosel_third_image_").src = firstProduct.first_mouse_carosel_third_image_;
+document.getElementById("first_mouse_carosel_fourth_image_").src = firstProduct.first_mouse_carosel_fourth_image_;
+
+document.getElementById("first_mouse_Qunatity").innerText = firstProduct.first_mouse_Qunatity;
+document.getElementById("first_mouse_orignal_price").innerText = firstProduct.first_mouse_orignal_price;
+document.getElementById("first_mouse_cut_price").innerText = firstProduct.first_mouse_cut_price;
+
+
+}
+
+
+// -------------------------------------------CLOSEEEEEEEEEEEEEEEEE--------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function mouse_1() {
 
     let data = {
@@ -91,7 +274,7 @@ function mouse_1() {
 
 
 
-function mouse_2(){
+function mouse_2() {
 
     let data_ = {
         "products": [
@@ -175,7 +358,7 @@ function mouse_2(){
 // product_3
 
 
-function Mouse_3(){
+function Mouse_3() {
 
     let data_3 = {
         "products": [
@@ -259,17 +442,17 @@ function Mouse_3(){
 
 
 // Page load hone pe automatically function call hoga
-window.onload = function() {
+window.onload = function () {
     const urlParams = new URLSearchParams(window.location.search);
     const productId = urlParams.get('product');
-    
-    if(productId === 'mouse_1') {
+
+    if (productId === 'mouse_1') {
         mouse_1();
 
-    } else if(productId === 'mouse_2') {
+    } else if (productId === 'mouse_2') {
         mouse_2();
-        
-    }else if(productId === 'mouse_3'){
+
+    } else if (productId === 'mouse_3') {
         Mouse_3();
 
     }
